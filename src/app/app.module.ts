d8110118cab3d94b74449c78a67399e5f305b716
kraenbyskov <kraenbyskov@gmail.com>
2020-03-31 15:55:24 +0200

    lets test it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { AppComponent } from "./app.component";
 import { ContentfulService } from "./contentful.service";
 import { RouterModule, Routes } from "@angular/router";
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import { environment } from "../environments/environment";
+
 import { HomeComponent } from "./pages/home/home.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
@@ -15,7 +22,7 @@ import { ContactComponent } from "./pages/contact/contact.component";
 import { AboutComponent } from "./pages/about/about.component";
 import { PostsComponent } from "./pages/posts/posts.component";
 
-const routes: Routes = [
+const appRoutes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "post/:id", component: PostsComponent },
@@ -34,7 +41,17 @@ const routes: Routes = [
     AboutComponent,
     PostsComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
   providers: [ContentfulService],
   bootstrap: [AppComponent]
 })
