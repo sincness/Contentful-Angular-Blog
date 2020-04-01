dceb1f64bb2c04a2ca78b232ca82e162d8997dad
kraenbyskov <kraenbyskov@gmail.com>
2020-04-01 11:36:00 +0200

    Will it blend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQ-0pEb_TEKE6oTgFFMMIdsiSktnZtxM0",
+    authDomain: "contentful-angular-blog.firebaseapp.com",
+    databaseURL: "https://contentful-angular-blog.firebaseio.com",
+    projectId: "contentful-angular-blog",
+    storageBucket: "contentful-angular-blog.appspot.com",
+    messagingSenderId: "1089846297065",
+    appId: "1:1089846297065:web:20c41165464c5e6cb4cb78",
+    measurementId: "G-0FCW1CMPMT"
+  },
+  contentful: {
+    space: "kn33bkhab8tl",
+    accessToken: "t75ExREYp9Y8UFPb94lzZcfgHfY2p3Pq9fNEc6e-kK8"
+  }
 };
